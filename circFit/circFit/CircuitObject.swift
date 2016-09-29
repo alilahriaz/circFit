aa9dc8ccb5fe3e390ad176c5f1e2fb1b1fb1c473
Ali Riaz <syedaliriaz@gmail.com>
2016-09-28 22:12:32 -0500

    AR: Added UIPickerView for workout type, extracted enum for workout and rest

--- circFit/circFit/CircuitObject.swift
+++ circFit/circFit/CircuitObject.swift
@@ -12,9 +12,40 @@ class CircuitObject: NSObject {
 
     var workoutName : String?
     var duration : Int?
+    var type : exerciseType
     
-    init(workoutName:String, duration:Int) {
+    init(workoutName:String, duration:Int, type:exerciseType) {
         self.workoutName = workoutName
         self.duration = duration
+        self.type = type
     }
+}
+
+enum exerciseType: Int, CustomStringConvertible {
+    case Workout = 0
+    case Rest = 1
+    
+    static var count: Int { return exerciseType.Rest.rawValue + 1 }
+    
+    var description: String {
+        switch self {
+        case .Workout:
+            return "Workout"
+        case .Rest:
+            return "Rest"
+        }
+    }
+    
+    init?(value: Int) {
+        switch value {
+        case 0:
+            self = .Workout
+        case 1:
+            self = .Rest
+        default:
+            return nil
+        }
+    }
+    
+    
 }
--- circFit/circFit/CreateCircuitViewController.swift
+++ circFit/circFit/CreateCircuitViewController.swift
@@ -8,21 +8,33 @@
 
 import UIKit
 
-class CreateCircuitViewController: UIViewController {
+class CreateCircuitViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     var circuitExcercises : [CircuitObject] = []
     let cellIdentifier = String(CircuitCollectionViewCell)
+    var currentExerciseType : exerciseType = .Workout
 
+// MARK: IBOutlets
     @IBOutlet weak var circuitCollectionView: UICollectionView!
     @IBOutlet weak var exerciseNameTextField: UITextField!
     @IBOutlet weak var durationTextField: UITextField!
+    @IBOutlet weak var exerciseTypePickerView: UIPickerView!
     
+// MARK: UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.exerciseTypePickerView.delegate = self;
+        self.exerciseTypePickerView.dataSource = self;
+    }
+    
+// MARK: IBActions
     @IBAction func enterButtonPressed(sender: UIButton) {
         
         if let exerciseName = self.exerciseNameTextField?.text! {
             if let duration = Int(self.durationTextField.text!) {
                 
-                let newCircuitEntry = CircuitObject(workoutName: exerciseName , duration : duration)
+                let newCircuitEntry = CircuitObject(workoutName: exerciseName , duration : duration, type: currentExerciseType)
                 self.circuitExcercises.append(newCircuitEntry)
                 
                 self.circuitCollectionView.reloadData()
@@ -35,8 +47,32 @@ class CreateCircuitViewController: UIViewController {
             print("Please add an exercise name")
         }
     }
+    
+// MARK: UIPickerView DataSource
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return exerciseType.count
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+// MARK: UIPickerView Delegate
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let type = exerciseType(rawValue: row)
+        self.currentExerciseType = type!
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let type = exerciseType(rawValue: row)
+        return type?.description
+    }
 }
 
+//UICollectionView DataSource
 extension CreateCircuitViewController : UICollectionViewDataSource {
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -53,6 +89,7 @@ extension CreateCircuitViewController : UICollectionViewDataSource {
     }
 }
 
+//UICollectionViewFlow Delegate
 extension CreateCircuitViewController : UICollectionViewDelegateFlowLayout {
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
